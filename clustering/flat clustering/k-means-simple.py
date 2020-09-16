b7982a7df4e334557af083a4cace395c1fbf6290
sinabr <sina.br.shirazu@gmail.com>
2020-09-16 12:59:07 +0430

    basic K-means Algorithm from scratch code added

---
+++ clustering/flat clustering/k-means-simple.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+from sklearn.cluster import KMeans
+
+x = np.array([[1,2],[1.5,1.8],[5,8],[8,8],[1,0.6],[9,11]])
+
+
+# x[:,0] -> all the 0th elements in x
+# plt.scatter(x[:,0],x[:,1],s=150,linewidths=5)
+# plt.show()
+
+clf = KMeans(n_clusters=2)
+
+clf.fit(x)
+
+centroids = clf.cluster_centers_
+
+labels = clf.labels_
+
+colors = ["r.","b."]
+
+for i in range(len(x)):
+    plt.plot(x[i][0] , x[i][1] , colors[labels[i]] , markersize = 10)
+
+plt.scatter(centroids[:,0] , centroids[:,1])
+plt.show()
